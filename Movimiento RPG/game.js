5d62a653d8f7805cb3dd837c27cc8bcfabbf2e24
Manuel Gutierrez <manga.coder.crafter@gmail.com>
2024-07-21 01:35:21 -0300

    Movimiento hecho :D

--- Movimiento RPG/game.js
+++ Movimiento RPG/game.js
@@ -4,6 +4,14 @@ const config = {
     type: Phaser.AUTO,
     width: 960,
     height: 960,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y : 0 },
+            debug: true
+        }
+    },
     scene: [Example]
 };
 
